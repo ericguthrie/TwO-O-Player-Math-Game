355a758975fdd98f533ba2d0eb3331a74db0436b
ericguthrie <ericguthrie12@gmail.com>
2021-04-26 23:46:13 +0000

    added players and lives logic

--- players_class.rb
+++ players_class.rb
@@ -1,10 +1,15 @@
 class Player 
-  attr_accessor :name
-  @@number_of_lives = 3
+  attr_accessor :player1, :player2
+  @@num_of_lives = 3
 
-
-  def initialize(n)
-    @name = n 
-  
+  def initialize(player1, player2)
+    @player1 = player1
+    @player2 = player2 
+    @@num_of_lives -= 1
   end
+
+  def lose_a_life
+    @@num_of_lives
+  end
+
 end
